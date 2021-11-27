b432fe4dcc14dccdb589a256f8912a0889f8fe2c
Varun Wahi <wahivarun02@gmail.com>
2021-11-27 06:27:09 +0530

    added 5 to the thing

--- src/Learners/Main.java
+++ src/Learners/Main.java
@@ -1,13 +1,16 @@
 package Learners;
 
+import java.util.Scanner;
+
 public class Main {
     private static int o = 10;
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         System.out.println(returnString("Varun's JEE Rank: "));
     }
     static String returnString(String str){
-        str+= "1234";
+        str+= "12345";
         return str;
     }
 
