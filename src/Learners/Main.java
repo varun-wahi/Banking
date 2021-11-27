a3c20367a8b7ec2ba479a75f536bee55f34e857e
Varun Wahi <wahivarun02@gmail.com>
2021-11-27 06:13:50 +0530

    First commit

---
+++ src/Learners/Main.java
@@ -0,0 +1,15 @@
+package Learners;
+
+public class Main {
+    private static int o = 10;
+
+    public static void main(String[] args) {
+        System.out.println(returnString("Varun's JEE Rank: "));
+    }
+    static String returnString(String str){
+        str+= "1234";
+        return str;
+    }
+
+}
+
