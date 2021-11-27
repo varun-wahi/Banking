498ead391d92527e835a444cc7ecbaad11895e08
Varun Wahi <wahivarun02@gmail.com>
2021-11-27 07:19:02 +0530

    * Changed package to "Banking" * Added a Main Menu * Added a basic working model

--- src/Banking/Main.java
+++ src/Banking/Main.java
@@ -4,14 +4,46 @@ import java.util.Scanner;
 
 public class Main {
     private static int o = 10;
+    public static int money = 0;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println(returnString("Varun's JEE Rank: "));
+        boolean running = true;
+        while (running){
+            mainMenu();
+            System.out.print("\nEnter your choice: ");
+            int choice = in.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    System.out.print("Enter the amount to deposit: ");
+                    int addAmount = in.nextInt();
+                    money += addAmount;
+                    System.out.println("\nRs." + addAmount + " added successfully!");
+                    System.out.println("Current Balance: " + money);
+                }
+                case 2 -> {
+                    System.out.print("Enter the amount to withdraw: ");
+                    int reduceAmount = in.nextInt();
+                    money -= reduceAmount;
+                    System.out.println("\nRs." + reduceAmount + " withdrawn successfully!");
+                    System.out.println("Current Balance: " + money);
+                }
+                case 3 -> System.out.println("\nCurrent Balance: " + money);
+                case 4 -> {
+                    System.out.println("Thank you for using our services! We'll see you soon!");
+                    running = false;
+                }
+            }
+        }
+
+
     }
-    static String returnString(String str){
-        str+= "12345";
-        return str;
+    static void mainMenu(){
+        System.out.println("\n" + "*".repeat(30));
+        System.out.println("1.Deposit Money");
+        System.out.println("2.Withdraw Money");
+        System.out.println("3.Check Balance");
+        System.out.println("4.Exit");
     }
 
 }
